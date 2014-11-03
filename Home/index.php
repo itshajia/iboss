08a9fb8450675f28d0991bab64e2f837a2f64190
itshajia <itshajia>
2014-11-03 09:38:40 +0800

    同步测试5

--- Home/index.php
+++ Home/index.php
@@ -2,6 +2,7 @@
 define('APP_NAME', 'Home');
 define('APP_PATH',dirname(__FILE__).'/');
 define('APP_DEBUG', true);
+echo 'Hello world4!!';
 echo 'Hello World5!!';
 
 require dirname(dirname(__FILE__)).'/ThinkPHP/ThinkPHP.php';
